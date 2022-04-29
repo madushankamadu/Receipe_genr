197a6e95d7d9c8a36068b7a5177d4d9cd7e999ac
Nadun Madhushanka <madushankaforgit@gmail.com>
2022-04-29 09:55:22 +0530

    Mobile application first part

---
+++ m-application/RecipeGenerator/app/src/main/java/com/example/recipegenerator/ResponsePOJO.java
@@ -0,0 +1,15 @@
+package com.example.recipegenerator;
+
+public class ResponsePOJO {
+
+    private boolean status;
+    private String remarks;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+}
